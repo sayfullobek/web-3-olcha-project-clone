f39fb802130b5ae7e624a70a044ff4b167539ea8
Azizbek-dev-ful <azizbekazizbek2033@gmail.com>
2023-05-23 19:45:00 +0500

    aziz

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import SignUp from "./Olcha/SignUp";
 import {NavBar} from "./nav/navBar";
 import Footer from "./nav/Footer";
+import { Olcha } from "./Olcha/Olcha";
+import Buyurtma from "./Olcha/Buyurtma";
+import Kuz from "./Olcha/Kuz";
+import Manzil from "./Olcha/Manzil";
+import Sevimli from "./Olcha/sevimli";
+import Bildi from "./Olcha/bildi";
 function App() {
     return (
         <BrowserRouter>
@@ -10,6 +16,12 @@ function App() {
             <Route path={"/to'lov"} element={<Tolov/>}/>
             <Route path={"/com"} element={<Sharx/>}/>
             <Route path={"/comen"} element={<Sharx2/>}/>
+            <Route path="/olcha" element={<Olcha/>}/>
+            <Route path="/Buyurtma" element={<Buyurtma/>}/>
+            <Route path="/Kuz" element={<Kuz/>}/>
+            <Route path="/Manzil" element={<Manzil/>}/>
+            <Route path="/sevimli" element={<Sevimli/>}/>
+            <Route path="/bildi" element={<Bildi/>}/>
             <Footer/>
         </BrowserRouter>
     );
